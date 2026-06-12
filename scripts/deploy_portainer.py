8dfebc2a2067b130223fd839aaaf87c8c5363c10
Michael Dziegiel <mdziegiel74@yahoo.com>
2026-06-12 04:36:23 +0000

    fix: verify live bundle feature strings

--- scripts/deploy_portainer.py
+++ scripts/deploy_portainer.py
@@ -137,7 +137,7 @@ else:
 
 with urllib.request.urlopen(f'http://{HOST}:10233/', timeout=20) as resp:
     html=resp.read(20000).decode('utf-8','replace')
-asset_matches=re.findall(r'/assets/[^"\']+\\.js', html)
+asset_matches=re.findall(r'/assets/[^"\']+\.js', html)
 bundle_text=''
 for asset in asset_matches[:5]:
     try:
